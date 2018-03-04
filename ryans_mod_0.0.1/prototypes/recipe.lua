3567f14ec0960bb2ea72e7ed7d665ecd5eaad30a
Ryan Grange <rgrange@protonmail.com>
2018-03-04 09:44:33 +0000

    First Push
    Factorio Mod Created in Canjam 2018

---
+++ ryans_mod_0.0.1/prototypes/recipe.lua
@@ -0,0 +1,15 @@
+data:extend({
+  {
+    type = "recipe",
+    name = "copper-wall",
+	energy_required = 2,
+    enabled = false,
+    ingredients = 
+	{
+	   {"copper-plate", 15},
+	   {"wall", 1}
+	},
+    result = "copper-wall",
+    requester_paste_multiplier = 10
+  },
+  })
